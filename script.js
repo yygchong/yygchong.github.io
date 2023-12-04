999693080d1d77da8f431d235396b3fc4620b10f
yygchong <34783474+yygchong@users.noreply.github.com>
2023-12-03 20:49:37 -0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,163 @@
+// Wait for the document to be fully loaded
+document.addEventListener("DOMContentLoaded", function () {
+        
+    // Select the title element
+    const title = document.getElementById("fade-in-title");
+
+    const titles = ["welcome to my website on the {world wide web} \u{1F30E} \u{1F9A4} \u{1F44D}", "Yong Chong \u{1F52E} 庄永扬"]; // Define your titles
+    let currentIndex = -1;
+
+    function fadeInNewTitle() {
+        if (currentIndex >= titles.length - 1) {
+            return;
+        }
+        // Add the 'hidden' class to hide the current title
+        title.classList.add("hidden");
+
+        // After a short delay, update the title content and remove the 'hidden' class to fade it in
+        setTimeout(function () {
+            title.textContent = titles[currentIndex];
+            title.classList.add("visible");
+        }, 600); // Adjust the delay (in milliseconds) as needed
+
+        //Fade out
+        if (currentIndex < titles.length - 2) {
+            setTimeout(function () {
+                title.classList.remove("visible");
+            }, 2500); // Adjust the delay (in milliseconds) as needed
+        }
+        currentIndex = (currentIndex + 1) % titles.length;
+        
+        
+        // Repeat the process after a delay
+        setTimeout(fadeInNewTitle, 3000); // Adjust the delay for title change
+
+    }
+
+    function fadeInBody() {
+        const body = document.getElementById("fade-in-body");
+        body.classList.add("visible");
+
+    }
+
+    // Start the process
+    fadeInNewTitle();
+});
+
+const workVideoPlayer = document.getElementById('workVideoPlayer');
+const workVideoSource = document.getElementById('workVideoSource');
+const workTextOverlay = document.getElementById('workTextOverlay');
+const workDetailsOverlay = document.getElementById('workDetailsOverlay');
+const workMetaDetails = document.getElementById('workMetaDetails');
+
+const tiktokExperience = `
+    <p> 
+    <b> Site Reliability Engineer (Global Payments) </b> <br> <i> 09.2022 - present </i> <br> <br>
+    - Worked in a team of 3 to lead the deployments of payment infrastructure in the successful launch of the e-commerce TikTok Shop in the US, collaborating closely with international developers to debug issues, modify code, and ensure continuous uptime for one of TikTok's largest revenue streams.<br>
+    - Headed the US payments team in the deployment of our US services onto a new datacenter in Chicago, ensuring compliance and stability of the platform while balancing resource allocation requirements.<br>
+    - Collaborated with developers from the US and international teams to resolve code issues and debug numerous services as well as collectively strategizing architecture to refine the optimal approaches for advancing our data synchronization platform. Established infrastructure for these services, including setting up MQ, RDS, ES, Data Syncing, Multi Tenant Clusters, Redis, SQL, and more. This includes creating a comprehensive environment for our new data center and implementing a segregated PCI cluster to safeguard sensitive data.<br>
+    - Developed internal SRE (Site Reliability Engineering) faas applications, including chat bots, internal scripts, aggregate error logging and testing automation for US Payment services with Python and Go.<br>
+    - Managed general SRE and infrastructure needs including deploying production services with CI/CD model, shard migrations, stress testing, and 24/7 on-call rotations. Successfully resolved critical issues within a complex microservice environment, such as P0 outages in payment channels, showcasing exceptional problem-solving skills and ensuring the reliability of high-stakes services. Additionally monitored the health of clusters and services using tools like Grafana and Kibana, while supporting the log distribution environment across various components. Also assumed ownership of critical Customer Platform components for the US team, including Capital accounts, Merchant statements, and Fee processing<br>
+    </p>`;
+const ibmExperience = `
+    <p> 
+    <b> Cloud Automation Developer </b> <br> <i> 01.2021 - 09.2022 </i> <br> <br>
+    - Developed scripts for internal testing and automation using Python, Bash, Selenium, Locust (Python), Ansible, and GraphQL. These scripts enhanced testing efficiency and automated various tasks, contributing to improved productivity and reliability.<br>
+    - Developed standardized and highly scalable load testing framework for our new ADP CloudPak Product, ensuring reliable performance and scalability under heavy user loads.<br>
+    - Deployed end-to-end Kubernetes (OCP) environments and conducted manual and automated debugging using Jenkins BVT pipelines, ensuring reliable and efficient deployments.<br>
+    - Built out dashboards which process raw test data to display regressions using Docker and chart.js (Javascript, Python)<br>
+    Verified and reported defects on each iteration of builds with end to end testing of the product<br>
+    Verified, reported, and debugged defects in each iteration of builds through comprehensive end-to-end testing of the product. Led the QA effort in implementing features such as SCIM/IdP functionality.<br>
+    - Collaborated with developers and cross-functional teams across geographical regions to troubleshoot issues, integrate code, and provide support during build launches.<br>
+    </p>`;
+const vspExperience = `
+    <p> 
+    <b> Site Reliability Engineer </b> <br> <i> 06.2019 - 12.2020 </i> <br> <br>
+    - Monitored and diagnosed application outages with Splunk and Nagios, including HTTP and REST applications.<br>
+    - Worked with Ansible plays for automating toil.<br>
+    - Set-up WAS9 infrastructure for new applications.<br>
+    - Completed Linux Sys Admin service requests.<br>
+    - Deployed non-prod applications.<br>
+    - Worked with Agile Scrum Framework, JIRA, ServiceNow, AWX and Git (BitBucket).<br>
+    </p>`;
+const ucdavisExperience = `
+    <p> 
+    <b> Web Developer </b> <br> <i> 12.2017 - 01.2020 </i> <br> <br>
+    - Web Developer and IT Staff for the lab of Dr. Suad Joseph. <br>
+    - Used Plone, HTML, Python, PHP and JS.<br>
+    - Additional responsibility as an acting lab manager 3 days a week.<br>
+    - (https://sjoseph.ucdavis.edu/)<br>
+    </p>`;
+const workVersions = [
+    { video: 'assets/work_experience1.mp4', text: 'TikTok', details: 'Work Experience', metaDetails: tiktokExperience},
+    { video: 'assets/work_experience2.mp4', text: 'IBM', details: 'Work Experience', metaDetails: ibmExperience},
+    { video: 'assets/work_experience3.mp4', text: 'VSP', details: 'Work Experience', metaDetails: vspExperience},
+    { video: 'assets/work_experience4.mp4', text: 'UC Davis', details: 'Work Experience', metaDetails: ucdavisExperience }
+    // Add more workVersions as needed
+];
+let currentWorkVersionIndex = 0;
+
+// Function to change the video source and text content
+function changeWorkVersion() {
+    currentWorkVersionIndex = (currentWorkVersionIndex + 1) % workVersions.length;
+    workVideoSource.src = workVersions[currentWorkVersionIndex].video;
+    workTextOverlay.textContent = workVersions[currentWorkVersionIndex].text;
+    workDetailsOverlay.textContent = workVersions[currentWorkVersionIndex].details;
+    workMetaDetails.innerHTML = workVersions[currentWorkVersionIndex].metaDetails;
+    workVideoPlayer.load();
+    workVideoPlayer.play();
+}
+
+const sachacksProject = `<p> 
+    <b> President + Co-Founder </b> <br> <i> 11.2018 - 03.2020 </i> <br> <br>
+    - Led a team of 40+ people with the mission to create Sacramento's second annual intercollegiate Hackathon, an event aimed towards creating a lasting impact on young engineers, designers and entrepreneurs around the Sacramento region. <br>
+    - Organized meetings and worked directly with partners across different channels including officials from MLH, from schools such as UC Davis, and from corporate sponsors such as IBM and the Sacramento Kings (NBA).<br>
+    - Managed team leads in Finance, Design and Operations by distributing tasks and consistently meeting with them in order to learn about their progress and to provide assistance for obstacles.<br>
+    - Directly oversaw a team which raised over $35,000 in funding.<br>
+    <br>
+    <br>
+    <b> Head of Finance + Co-Founder </b> <br> <i> 01.2018 - 11.2018 </i> <br> <br>
+    - Headed a team of more than 15 people
+    - Distributed leads for sponsorship efforts for the inaugural SacHacks Hackathon 2018. 
+    - Oversaw the challenging task of raising $30,000 for the first collegiate Hackathon in Sacramento. - Worked closely with companies and organizations such as EA, the NBA (Sacramento Kings), Wolfram, Github, Bloomberg, MLH, Nvidia, Soylent and Google.
+    </p>`;
+
+const locknwalkProject = `<p> 
+    <b> Co-Project Lead </b> <br> <i> 04.2019 - 01.2020 </i> <br> <br>
+    - Developed an innovative skateboard rack that is more secure than current designs on the market. <br>
+    - Won $1,000 in the Little Bang Startup competition to put towards prototyping. <br>
+    - Coordinated with UC Davis TAPS to test at the UC Davis Activities and Recreation center. <br>
+    - Co-Project Lead: Jonathan Bryant <br>
+    </p>`;
+
+const heliostatProject = `<p> 
+    <b> Personal Project </b> <br> <i> 04.2019 - 01.2020 </i> <br> <br>
+    - Developed a heliostat that tracks the sun and reflects light into a room. <br>
+    </p>`;
+
+const cstemProject = `<p> 
+    <b> Curriculum Developer </b> <br> <i> 11.2017 - 10.2018 </i> <br> <br>
+    - Developed ChIDE solutions for the RoboPlay Competition, programming a series of 20 unique Barobo sequences. Edited and directed all the official video content for 2017 - 2018 RoboPlay Competition. <br>
+    - Coordinated the 2018 RoboPlay Video Play Competition, which included hundreds of participants from dozens of K-14 school districts. <br> <br>
+     <iframe width="560" height="315" src="https://www.youtube.com/embed/cnTzZDkMYXM" frameborder="0" allowfullscreen></iframe> <br>
+    </p>`;
+
+let currentProjectVersionIndex = 0;
+
+const projectVersions = [
+    { video: 'assets/project_experience1.mp4', text: 'SacHacks', details: 'My Projects', metaDetails: sachacksProject},
+    { video: 'assets/project_experience2.mp4', text: 'Lock and Walk', details: 'My Projects', metaDetails: locknwalkProject},
+    // { video: 'project_experience1.mp4', text: 'N/A', details: 'Volunteering', metaDetails: heliostatProject},
+    { video: 'assets/project_experience4.mp4', text: 'C-STEM', details: 'Volunteering', metaDetails: cstemProject }
+    // Add more workVersions as needed
+];
+
+function changeProjectVersion() {
+    currentProjectVersionIndex = (currentProjectVersionIndex + 1) % projectVersions.length;
+    projectVideoSource.src = projectVersions[currentProjectVersionIndex].video;
+    projectTextOverlay.textContent = projectVersions[currentProjectVersionIndex].text;
+    projectDetailsOverlay.textContent = projectVersions[currentProjectVersionIndex].details;
+    projectMetaDetails.innerHTML = projectVersions[currentProjectVersionIndex].metaDetails;
+    projectVideoPlayer.load();
+    projectVideoPlayer.play();
+}
